6895c174e1970b8d65e237e120564318ebdea0db
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-26 10:19:45 -0500

    Add helper function comments and write helper functions to create field selectors

--- translator.py
+++ translator.py
@@ -1,9 +1,55 @@
+# Helper function to parse list of fields to select
+def fieldsList(fieldsString):
+  fields = fieldsString.split(',')
+  fieldNames = []
+
+  for field in fields:
+    colon = field.find(":")
+    if field[colon + 1:] == '1':
+      fieldNames.append(field[:colon])
+  
+  return fieldNames
+
+# Helper function to stringify fields for appending to SQL query
+def stringifyFields(fieldsString):
+  fields = fieldsList(fieldsString)
+  return ",".join(fields)
+
+# Helper function to separate WHERE and FIELDS arguments
+def whereAndFieldsSeparator(arguments):
+  arguments = arguments.replace("(","").replace(")","").replace(";","").replace(" ","")
+  return arguments[1:-1].split("},{")
+
+# Helper function to destructure method from arguments
+def destructureMethod(call):
+  argumentStart_index = call.find('(')
+  method = call[:argumentStart_index]
+  arguments = call[argumentStart_index:]
+
+  return [method, arguments]
+
+# Helper function to destructure db, table and method call
 def destructureQuery(query):
   return query.split('.')
 
+# Main function to translate MongoDB to SQL
 def translator(mongoQuery):
-  db, table, method = destructureQuery(mongoQuery)
-  return method
+  db, table, methodCall = destructureQuery(mongoQuery)
+
+  method, combinedArguments = destructureMethod(methodCall)
+
+  separatedArguments = whereAndFieldsSeparator(combinedArguments)
+
+  whereArguments = separatedArguments[0]
+
+  fieldsSQL = "*" if len(separatedArguments) == 1 else stringifyFields(separatedArguments[1])
+
+  print(whereArguments)
+  print(fieldsSQL)
+
+  # whereClause = ""
+
+  # return f'SELECT {fieldsSQL} FROM {table}' + whereClause
 
 
 
@@ -14,5 +60,6 @@ def translator(mongoQuery):
 example1 = "db.user.find({name:'julio'});"
 example2 = "db.user.find({_id:23113},{name:1,age:1});"
 example3 = "db.user.find({age:{$gte:21}},{name:1,_id:1});"
+example4 = "db.user.find({age:{$gte:21,$lte:50}});"
 
-print(translator(example1))
+translator(example1)
