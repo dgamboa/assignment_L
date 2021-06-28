8e2b922d4e52dbe3564a2d543c09d932fa06bce2
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-28 09:10:48 -0500

    Remove examples from translator main file

--- translator/main.py
+++ translator/main.py
@@ -25,36 +25,3 @@ def translate(mongoQuery):
 
   # Return the combined string for the translated SQL query
   return f'SELECT {fieldsSQL} FROM {table}{whereSQL};'
-
-
-
-
-# ******************************* Manual Tests ******************************* #
-# example1 = "db.user.find({name:'julio'});"
-# example2 = "db.user.find({_id:23113},{name:1,age:1});"
-# example3 = "db.user.find({age:{$gte:21}},{name:1,_id:1});"
-# example4 = "db.user.find({age:{$gte:21,$lte:50}});"
-# example5 = "db.user.find({age:{$gte:21,$lte:50},name:'julio'});"
-# example6 = "db.user.find({age:20,name:'julio'});"
-# example7 = "db.user.find({},{name:1,age:1});"
-# example8 = "db.user.find({},{name:1,age:1,_id:0});"
-# example9 = "db.user.find({$or:[{status:'A'},{age:50}]})"
-# example10 = "db.user.find({$or:[{status:'A'},{age:50}],name:'julio'},{name:1,age:1})" # -> SELECT name, age FROM user WHERE (status = 'A' OR age = 50) AND name = 'julio';
-# example11 = "db.user.find({age:{$in:[20,25]}})" # -> SELECT * FROM user WHERE age IN (20, 25);
-# example12 = "db.user.find({$or:[{status:'A'},{age:50}],$and:[{name:'julio'},{status:'active'}]})" # -> SELECT * FROM user WHERE (status = 'A' OR age = 50) AND (name = 'julio' AND status = 'active');
-# example13 = "db.user.watch({},{name:1,age:1});" # -> Raises method error
-
-# print(translate(example1))
-# print(translate(example2))
-# print(translate(example3))
-# print(translate(example4))
-# print(translate(example5))
-# print(translate(example6))
-# print(translate(example7))
-# print(translate(example8))
-# print(translate(example9))
-# print(translate(example10))
-# print(translate(example11))
-# print(translate(example12))
-# print(translate(example13)) -> error, not find
-# print(translate({"table": "user", "method": "find"})) -> error, not string
